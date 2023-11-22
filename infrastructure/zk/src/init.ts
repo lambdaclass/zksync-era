2a4c13a7ce0d133523a316ef2926fc809cbcd119
IAvecilla <iavecilla@fi.uba.ar>
2023-11-22 17:11:31 -0300

    Add flag to skip plonk setup

--- infrastructure/zk/src/init.ts
+++ infrastructure/zk/src/init.ts
@@ -21,6 +21,7 @@ export async function init(initArgs: InitArgs = DEFAULT_ARGS) {
     const {
         skipSubmodulesCheckout,
         skipEnvSetup,
+        skipPlonkStep,
         testTokens,
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         deployerL1ContractInputArgs,
@@ -33,7 +34,7 @@ export async function init(initArgs: InitArgs = DEFAULT_ARGS) {
         await announced('Checking environment', checkEnv());
         await announced('Checking git hooks', env.gitHooks());
         await announced('Setting up containers', up());
-        await announced('Checking PLONK setup', run.plonkSetup());
+        !skipPlonkStep && await announced('Checking PLONK setup', run.plonkSetup());
     }
     if (!skipSubmodulesCheckout) {
         await announced('Checkout system-contracts submodule', submoduleUpdate());
@@ -144,6 +145,7 @@ async function checkEnv() {
 export interface InitArgs {
     skipSubmodulesCheckout: boolean;
     skipEnvSetup: boolean;
+    skipPlonkStep: boolean;
     deployerL1ContractInputArgs: any[];
     governorPrivateKeyArgs: any[];
     deployerL2ContractInput: {
@@ -160,6 +162,7 @@ export interface InitArgs {
 const DEFAULT_ARGS: InitArgs = {
     skipSubmodulesCheckout: false,
     skipEnvSetup: false,
+    skipPlonkStep: false,
     deployerL1ContractInputArgs: [],
     governorPrivateKeyArgs: [],
     deployerL2ContractInput: { args: [], includePaymaster: true, includeL2WETH: true },
@@ -169,11 +172,13 @@ const DEFAULT_ARGS: InitArgs = {
 export const initCommand = new Command('init')
     .option('--skip-submodules-checkout')
     .option('--skip-env-setup')
+    .option('--skip-plonk-step')
     .description('perform zksync network initialization for development')
     .action(async (cmd: Command) => {
         const initArgs: InitArgs = {
             skipSubmodulesCheckout: cmd.skipSubmodulesCheckout,
             skipEnvSetup: cmd.skipEnvSetup,
+            skipPlonkStep: cmd.skipPlonkStep,
             deployerL1ContractInputArgs: [],
             governorPrivateKeyArgs: [],
             deployerL2ContractInput: { args: [], includePaymaster: true, includeL2WETH: true },
