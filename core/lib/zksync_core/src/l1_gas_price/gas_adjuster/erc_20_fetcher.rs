0f77ac957615f6283d95ffb4b2baaa2c440267d5
Francisco Krause Arnim <fkrausear@gmail.com>
2023-12-22 19:08:56 -0300

    Rename variable

--- core/lib/zksync_core/src/l1_gas_price/gas_adjuster/erc_20_fetcher.rs
+++ core/lib/zksync_core/src/l1_gas_price/gas_adjuster/erc_20_fetcher.rs
@@ -25,17 +25,19 @@ async fn fetch_it() -> Result<String, Error> {
 }
 
 fn erc20_value_from_eth_to_wei(value_in_eth: &str) -> Result<u64, String> {
-    let vec: Vec<&str> = value_in_eth.split(".").collect();
-    let whole_part: u64 = u64::from_str_radix(
-        vec.first()
+    let splitted_value: Vec<&str> = value_in_eth.split(".").collect();
+    let whole_part = u64::from_str_radix(
+        splitted_value
+            .first()
             .ok_or("Expected decimal value separated by coma")?,
         10,
     )
     .map_err(|_| "Expected decimal value separated by coma")?;
     let whole_part_in_wei = to_wei(whole_part, 0_u32);
-    let decimal_length = vec.last().unwrap().len() as u32;
+    let decimal_length = splitted_value.last().unwrap().len() as u32;
     let decimal_part = u64::from_str_radix(
-        vec.last()
+        splitted_value
+            .last()
             .ok_or("Expected decimal value separated by coma")?,
         10,
     )
