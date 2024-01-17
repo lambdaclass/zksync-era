9342b29b7c89e283df1ae355ff8ff78fdd8c344c
Joaquin Carletti <joaquin.carletti@lambdaclass.com>
2024-01-17 12:43:54 -0300

    add graph script

---
+++ validium_mode_example/scripts/crete_gas_report.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def main():
+    try:
+        df_rollup = pd.read_csv('../rollup_gas_report.csv', header=0)
+        df_validium = pd.read_csv('../validium_gas_report.csv', header=0)
+    except:
+        print("Error: Report not found.")
+        return
+
+    rollup_mean_transaction_gas = df_rollup.groupby('operation')['transaction_gas_used'].mean().reset_index()
+    validium_mean_transaction_gas = df_validium.groupby('operation')['transaction_gas_used'].mean().reset_index()
+
+    results_df = pd.merge(rollup_mean_transaction_gas, validium_mean_transaction_gas, on='operation', suffixes=('_rollup', '_validium'))
+
+    results_df.plot(x='operation', 
+        kind='bar', 
+        stacked=False) 
+    plt.savefig("graph")
+
+if __name__ == "__main__":
+    main()
