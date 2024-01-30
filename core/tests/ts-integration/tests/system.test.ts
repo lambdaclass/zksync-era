6cb9894dd90be616099e45e986f1dcc41f968171
Jordi <jordi.bonet.valiente@lambdaclass.com>
2024-01-30 15:30:43 +0100

    Format

--- core/tests/ts-integration/tests/system.test.ts
+++ core/tests/ts-integration/tests/system.test.ts
@@ -77,7 +77,7 @@ describe('System behavior checks', () => {
     test('Should accept transactions with small gasPerPubdataByte', async () => {
         // The number "10" was chosen because we have a different error for lesser `smallGasPerPubdata`.
         // In validium mode, this minimum value is "55"
-        const smallGasPerPubdata = SYSTEM_CONFIG['L1_GAS_PER_PUBDATA_BYTE'] > 0 ? 10: 55;
+        const smallGasPerPubdata = SYSTEM_CONFIG['L1_GAS_PER_PUBDATA_BYTE'] > 0 ? 10 : 55;
 
         // This tx should be accepted by the server, but would never be executed, so we don't wait for the receipt.
         await alice.sendTransaction({
