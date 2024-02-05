15a71f6e5dd1b1cb8126e18684ff5501a02dc1f1
toni-calvin <antonio.calvin@lambdaclass.com>
2024-02-05 16:46:42 +0100

    abstract methods and constants

---
+++ infrastructure/zk/src/constants.ts
@@ -0,0 +1,8 @@
+export const VALIDIUM_PUBDATA_OVERHEAD_PART: number = 0.0;
+export const ROLLUP_PUBDATA_OVERHEAD_PART: number = 0.0;
+export const VALIDIUM_BATCH_OVERHEAD_L1_GAS: number = 1000000;
+export const ROLLUP_BATCH_OVERHEAD_L1_GAS: number = 800000;
+export const VALIDIUM_MAX_PUBDATA_PER_BATCH: number = 1000000000000;
+export const ROLLUP_MAX_PUBDATA_PER_BATCH: number = 100000;
+export const VALIDIUM_ENFORCED_L1_GAS_PRICE: number = 45_000_000_000;
+export const ROLLUP_ENFORCED_L1_GAS_PRICE = null;
