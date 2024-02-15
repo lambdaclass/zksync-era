477df6b1a2f0c583dbab2282151087f57238197f
ilitteri <ilitteri@fi.uba.ar>
2024-02-15 13:00:21 -0300

    Fix updateConfigFile

--- infrastructure/zk/src/init.ts
+++ infrastructure/zk/src/init.ts
@@ -137,16 +137,27 @@ export async function submoduleUpdate() {
     await utils.exec('git submodule update');
 }
 
-function updateConfigFile(path: string, modeConstantValues: any) {
+function updateConfigFile(path: string, modeConstantValues: Record<string, number | string | null>) {
     let content = fs.readFileSync(path, 'utf-8');
     let lines = content.split('\n');
-    let entries = Object.entries(modeConstantValues);
-    let addedContent;
-    while (entries.length > 0) {
-        const [key, value] = entries.pop()!;
-        let lineIndex = lines.findIndex((line) => !line.startsWith('#') && line.includes(`${key}=`));
+    let addedContent: string | undefined;
+    const lineIndices: Record<string, number> = {};
 
-        if (lineIndex !== -1) {
+    for (let i = 0; i < lines.length; i++) {
+        const line = lines[i];
+        if (!line.startsWith('#')) {
+            const match = line.match(/([^=]+)=(.*)/);
+            if (match) {
+                const key = match[1].trim();
+                lineIndices[key] = i;
+            }
+        }
+    }
+
+    for (const [key, value] of Object.entries(modeConstantValues)) {
+        const lineIndex = lineIndices[key];
+
+        if (lineIndex !== undefined) {
             if (value !== null) {
                 lines.splice(lineIndex, 1, `${key}=${value}`);
             } else {
@@ -163,10 +174,13 @@ function updateConfigFile(path: string, modeConstantValues: any) {
             }
         }
     }
+
     content = lines.join('\n');
+
     if (addedContent) {
         content += addedContent;
     }
+
     fs.writeFileSync(path, content);
 }
 function updateChainConfig(validiumMode: boolean) {
