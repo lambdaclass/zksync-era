03e77d3e87ec5263c21201db5997339f61ac5e75
Joaquin Carletti <joaquin.carletti@lambdaclass.com>
2024-02-16 18:02:06 -0300

    improve tests

--- core/tests/ts-integration/tests/validium_example.test.ts
+++ core/tests/ts-integration/tests/validium_example.test.ts
@@ -11,66 +11,66 @@ import { deployContract, getTestContract } from '../src/helpers';
 
 import * as ethers from 'ethers';
 import * as zksync from 'zksync-web3';
-import { Provider } from 'zksync-web3';
-import { RetryProvider } from '../src/retry-provider';
-
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+import { shouldChangeTokenBalances, shouldOnlyTakeFee } from '../src/modifiers/balance-checker';
 
 const contracts = {
-    erc20: getTestContract('ERC20Validium'),
+    erc20: getTestContract('ERC20Validium')
 };
 
 describe('Smart contract behavior checks', () => {
     let testMaster: TestMaster;
     let alice: zksync.Wallet;
     let bob: zksync.Wallet;
+    let erc20: ethers.Contract
 
     beforeAll(() => {
         testMaster = TestMaster.getInstance(__filename);
         alice = testMaster.mainAccount();
         bob = testMaster.newEmptyAccount();
+        
     });
 
-    test('Should deploy validim-example contract', async () => {
-        const validimExampleContract = await deployContract(alice, contracts.erc20, ["LambdaToken", "lmd"]);
+    let validimExampleContract: zksync.Contract
+
+    test('Deploy', async () => {
+        validimExampleContract = await deployContract(alice, contracts.erc20, ['LambdaToken', 'lmd']);
         await expect(validimExampleContract.name()).resolves.toBe('LambdaToken');
 
         const response = await validimExampleContract.deployed();
         const deployHash = response.deployTransaction.hash;
 
         const deployRecipt = await alice.provider.getTransactionReceipt(deployHash);
-        console.log("Deploy: ", parseInt(deployRecipt.gasUsed._hex));
+        console.log('Deploy gas: ', parseInt(deployRecipt.gasUsed._hex));
+    });
 
-        let AliceBlance = await validimExampleContract.balanceOf(alice.address);
-        console.log("Alice Balance: ", parseInt(AliceBlance._hex))
-        let BobBalance = await validimExampleContract.balanceOf(bob.address);
-        console.log("Bob Balance: ", parseInt(BobBalance._hex))
+    test('Mint', async () => {
 
-        const mint = await validimExampleContract._mint(alice.address,100000);
-
-        await sleep(5000);
+        const value = 10000
+        const mint = await validimExampleContract._mint(alice.address, value);
+        await mint.waitFinalize();
 
         const mintRecipt = await alice.provider.getTransactionReceipt(mint.hash);
-        console.log("Mint: ", parseInt(mintRecipt.gasUsed._hex));
+        console.log('Mint gas: ', parseInt(mintRecipt.gasUsed._hex));
 
-        AliceBlance = await validimExampleContract.balanceOf(alice.address);
-        console.log("Alice Balance: ", parseInt(AliceBlance._hex))
-        BobBalance = await validimExampleContract.balanceOf(bob.address);
-        console.log("Bob Balance: ", parseInt(BobBalance._hex))
+        const balanceChange = await shouldChangeTokenBalances(validimExampleContract.address, [
+            { wallet: alice, change: value }
+        ]);
+    });
 
-
-        const trasnfer = await validimExampleContract.transfer(bob.address,1000);
-
-        await sleep(5000);
+    test('Transfer', async () => {
+        const value = 1000;
+        const trasnfer = await validimExampleContract.transfer(bob.address, value);
+        await trasnfer.waitFinalize();
 
         const transferRecipt = await alice.provider.getTransactionReceipt(trasnfer.hash);
-        console.log("Transfer: ", parseInt(transferRecipt.gasUsed._hex));
+        console.log('Transfer: ', parseInt(transferRecipt.gasUsed._hex));
 
-        AliceBlance = await validimExampleContract.balanceOf(alice.address);
-        console.log("Alice Balance: ", parseInt(AliceBlance._hex))
-        BobBalance = await validimExampleContract.balanceOf(bob.address);
-        console.log("Bob Balance: ", parseInt(BobBalance._hex))
-    })
+        const balanceChange = await shouldChangeTokenBalances(validimExampleContract.address, [
+            { wallet: alice, change: -value },
+            { wallet: bob, change: value }
+        ]);
+        const feeCheck = await shouldOnlyTakeFee(alice);
+    });
 
     afterAll(async () => {
         await testMaster.deinitialize();
