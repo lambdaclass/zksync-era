22a7140f05f2e0036da7685daeae740be423ad60
ilitteri <ilitteri@fi.uba.ar>
2024-03-05 22:42:05 -0300

    Update validium mode example

--- validium_mode_example/src/main.rs
+++ validium_mode_example/src/main.rs
@@ -1,13 +1,14 @@
 use std::{str::FromStr, time::Duration};
 
 use colored::Colorize;
-use ethers::{abi::Abi, providers::Http, utils::parse_units};
+use ethers::{
+    abi::Abi, core::k256::ecdsa::SigningKey, providers::Http, types::TransactionReceipt,
+    utils::parse_units,
+};
 use loadnext::config::LoadtestConfig;
 use tokio::time::sleep;
-use zksync_web3_decl::{
-    jsonrpsee::http_client::HttpClientBuilder,
-    namespaces::{EthNamespaceClient, ZksNamespaceClient},
-};
+use zksync_types::{api::TransactionDetails, H160, H256, U256};
+use zksync_web3_decl::{jsonrpsee::http_client::HttpClientBuilder, namespaces::ZksNamespaceClient};
 use zksync_web3_rs::{
     eip712::Eip712TransactionRequest,
     providers::{Middleware, Provider},
@@ -25,119 +26,87 @@ static CONTRACT_ABI: &str = include_str!("../ERC20.abi");
 
 static L1_URL: &str = "http://localhost:8545";
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() {
-    let l1_provider =
-        Provider::<Http>::try_from(L1_URL).expect("Could not instantiate L1 Provider");
-    let zk_wallet = {
-        let era_provider = Provider::try_from(ERA_PROVIDER_URL).unwrap();
+fn l1_provider() -> Provider<Http> {
+    Provider::<Http>::try_from(L1_URL).expect("Could not instantiate L1 Provider")
+}
 
-        let chain_id = era_provider.get_chainid().await.unwrap();
-        let l2_wallet = LocalWallet::from_str(PRIVATE_KEY)
-            .unwrap()
-            .with_chain_id(chain_id.as_u64());
-        ZKSWallet::new(
-            l2_wallet,
-            None,
-            Some(era_provider.clone()),
-            Some(l1_provider.clone()),
-        )
+fn l2_provider() -> Provider<Http> {
+    Provider::try_from(ERA_PROVIDER_URL).unwrap()
+}
+
+async fn zks_wallet(
+    l1_provider: &Provider<Http>,
+    l2_provider: &Provider<Http>,
+) -> ZKSWallet<Provider<Http>, SigningKey> {
+    let chain_id = l2_provider.get_chainid().await.unwrap();
+    let l2_wallet = LocalWallet::from_str(PRIVATE_KEY)
         .unwrap()
-    };
+        .with_chain_id(chain_id.as_u64());
+    ZKSWallet::new(
+        l2_wallet,
+        None,
+        Some(l2_provider.clone()),
+        Some(l1_provider.clone()),
+    )
+    .unwrap()
+}
 
-    let config = LoadtestConfig::from_env()
-        .expect("Config parameters should be loaded from env or from default values");
+async fn deposit(zks_wallet: &ZKSWallet<Provider<Http>, SigningKey>) -> H256 {
+    let amount = parse_units("11", "ether").unwrap();
+    let request = DepositRequest::new(amount.into());
+    zks_wallet
+        .deposit(&request)
+        .await
+        .expect("Failed to perform deposit transaction")
+}
 
-    let l1_rpc_client = HttpClientBuilder::default()
-        .build(config.l1_rpc_address)
+async fn deploy(zks_wallet: &ZKSWallet<Provider<Http>, SigningKey>) -> (H160, TransactionReceipt) {
+    println!("{}", "Deploy".bright_magenta());
+
+    // Read both files from disk:
+    let abi = Abi::load(CONTRACT_ABI.as_bytes()).unwrap();
+    let contract_bin = hex::decode(CONTRACT_BIN).unwrap().to_vec();
+
+    // DeployRequest sets the parameters for the constructor call and the deployment transaction.
+    let request = DeployRequest::with(
+        abi,
+        contract_bin,
+        vec!["ToniToken".to_owned(), "teth".to_owned()],
+    )
+    .from(zks_wallet.l2_address());
+
+    let eip712_request: Eip712TransactionRequest = request.clone().try_into().unwrap();
+
+    let l2_deploy_tx_receipt = zks_wallet
+        .get_era_provider()
+        .unwrap()
+        .clone()
+        .send_transaction_eip712(&zks_wallet.l2_wallet, eip712_request)
+        .await
+        .unwrap()
+        .await
+        .unwrap()
         .unwrap();
-    let l2_rpc_client = HttpClientBuilder::default()
-        .build(config.l2_rpc_address)
-        .unwrap();
 
-    let deposit_transaction_hash = {
-        let amount = parse_units("11", "ether").unwrap();
-        let request = DepositRequest::new(amount.into());
-        zk_wallet
-            .deposit(&request)
-            .await
-            .expect("Failed to perform deposit transaction")
-    };
-    println!();
-    println!("Deposit transaction hash: {:?}", deposit_transaction_hash);
+    (
+        l2_deploy_tx_receipt.contract_address.unwrap(),
+        l2_deploy_tx_receipt,
+    )
+}
 
-    // Deploy contract:
-    let contract_address = {
-        // Read both files from disk:
-        let abi = Abi::load(CONTRACT_ABI.as_bytes()).unwrap();
-        let contract_bin = hex::decode(CONTRACT_BIN).unwrap().to_vec();
-
-        // DeployRequest sets the parameters for the constructor call and the deployment transaction.
-        let request = DeployRequest::with(
-            abi,
-            contract_bin,
-            vec!["ToniToken".to_owned(), "teth".to_owned()],
-        )
-        .from(zk_wallet.l2_address());
-
-        let eip712_request: Eip712TransactionRequest = request.clone().try_into().unwrap();
-        println!("{}", "Deploy".bright_magenta());
-
-        let transaction_receipt = zk_wallet
-            .get_era_provider()
-            .unwrap()
-            .clone()
-            .send_transaction_eip712(&zk_wallet.l2_wallet, eip712_request)
-            .await
-            .unwrap()
-            .await
-            .unwrap()
-            .unwrap();
-
-        let address = transaction_receipt.contract_address.unwrap();
-        println!("Contract address: {:#?}", address);
-
-        let transaction_hash_deploy = transaction_receipt.transaction_hash;
-        let transaction_hash_formatted_deploy =
-            format!("{:#?}", transaction_receipt.transaction_hash);
-        println!("Transaction hash {}", transaction_hash_formatted_deploy);
-        let transaction_gas_used_formatted_deploy =
-            format!("{:#?}", transaction_receipt.gas_used.unwrap());
-        println!(
-            "Transaction gas used {}",
-            transaction_gas_used_formatted_deploy.cyan()
-        );
-        let l2_transaction_deploy = {
-            loop {
-                let l2_transaction = l2_rpc_client
-                    .get_transaction_details(transaction_hash_deploy)
-                    .await
-                    .unwrap()
-                    .unwrap();
-
-                if l2_transaction.eth_commit_tx_hash.is_some() {
-                    break l2_transaction.clone();
-                }
-
-                sleep(Duration::from_secs(1)).await;
-            }
-        };
-
-        let l2_tx_fee_formatted_deploy = format!("{:#?}", l2_transaction_deploy.fee);
-        println!("L2 fee: {}", l2_tx_fee_formatted_deploy.green());
-
-        address
-    };
-    println!();
-
+async fn mint(
+    zks_wallet: &ZKSWallet<Provider<Http>, SigningKey>,
+    erc20_address: H160,
+) -> TransactionReceipt {
     println!("{}", "Mint".bright_magenta());
-    let receipt_mint = zk_wallet
+
+    zks_wallet
         .get_era_provider()
         .unwrap()
         .clone()
         .send_eip712(
-            &zk_wallet.l2_wallet,
-            contract_address,
+            &zks_wallet.l2_wallet,
+            erc20_address,
             "_mint(address, uint256)",
             Some(
                 [
@@ -152,118 +121,144 @@ async fn main() {
         .unwrap()
         .await
         .unwrap()
-        .unwrap();
+        .unwrap()
+}
 
-    let transaction_hash_mint = receipt_mint.transaction_hash;
-    let transaction_hash_formatted_mint = format!("{:#?}", receipt_mint.transaction_hash);
-    println!("Transaction hash {}", transaction_hash_formatted_mint);
-    let transaction_gas_used_formatted_mint = format!("{:#?}", receipt_mint.gas_used.unwrap());
-    println!(
-        "Transaction gas used {}",
-        transaction_gas_used_formatted_mint.cyan()
-    );
-    let l2_transaction_mint = {
-        loop {
-            let l2_transaction = l2_rpc_client
-                .get_transaction_details(transaction_hash_mint)
-                .await
-                .unwrap()
-                .unwrap();
+async fn transfer(
+    zks_wallet: &ZKSWallet<Provider<Http>, SigningKey>,
+    erc20_address: H160,
+) -> TransactionReceipt {
+    println!("{}", "Transfer".bright_magenta());
 
-            if l2_transaction.eth_commit_tx_hash.is_some() {
-                break l2_transaction.clone();
-            }
-
-            sleep(Duration::from_secs(1)).await; // Adjust the duration as needed
-        }
-    };
-
-    let l2_tx_fee_formatted_mint = format!("{:#?}", l2_transaction_mint.fee);
-    println!("L2 fee: {}", l2_tx_fee_formatted_mint.green());
-
-    let l1_transaction_transfer = l1_rpc_client
-        .get_transaction_by_hash(l2_transaction_mint.eth_commit_tx_hash.unwrap())
+    zks_wallet
+        .get_era_provider()
+        .unwrap()
+        .clone()
+        .send_eip712(
+            &zks_wallet.l2_wallet,
+            erc20_address,
+            "_transfer(address, address, uint256)",
+            Some(
+                [
+                    "CD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826".into(),
+                    "bBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB".into(),
+                    "1000".into(),
+                ]
+                .into(),
+            ),
+            None,
+        )
         .await
         .unwrap()
+        .await
+        .unwrap()
+        .unwrap()
+}
+
+async fn wait_for_l2_tx_details(tx_hash: H256) -> TransactionDetails {
+    let config = LoadtestConfig::from_env()
+        .expect("Config parameters should be loaded from env or from default values");
+
+    let client = HttpClientBuilder::default()
+        .build(config.l2_rpc_address)
         .unwrap();
-
-    let l1_max_fee_per_gas_mint = l1_transaction_transfer.max_fee_per_gas.unwrap();
-    let l1_max_fee_per_gas_formatted_mint = format!("{:#?}", l1_max_fee_per_gas_mint);
-    println!(
-        "L1 max fee per gas: {}",
-        l1_max_fee_per_gas_formatted_mint.cyan()
-    );
-    println!();
-
-    let values: Vec<&str> = vec!["1000"];
-
-    for &value in &values {
-        println!("Transfer {}", value.bright_magenta());
-        let receipt_transfer = zk_wallet
-            .get_era_provider()
-            .unwrap()
-            .clone()
-            .send_eip712(
-                &zk_wallet.l2_wallet,
-                contract_address,
-                "_transfer(address, address, uint256)",
-                Some(
-                    [
-                        "CD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826".into(),
-                        "bBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB".into(),
-                        value.into(),
-                    ]
-                    .into(),
-                ),
-                None,
-            )
-            .await
-            .unwrap()
+    loop {
+        let details = client
+            .get_transaction_details(tx_hash)
             .await
             .unwrap()
             .unwrap();
 
-        let transaction_hash_transfer = receipt_transfer.transaction_hash;
-        let transaction_hash_formatted_transfer =
-            format!("{:#?}", receipt_transfer.transaction_hash);
-        println!("Transaction hash {}", transaction_hash_formatted_transfer);
-        let transaction_gas_used_formatted_transfer =
-            format!("{:#?}", receipt_transfer.gas_used.unwrap());
-        println!(
-            "Transaction gas used {}",
-            transaction_gas_used_formatted_transfer.cyan()
-        );
-        let l2_transaction_transfer = {
-            loop {
-                let l2_transaction = l2_rpc_client
-                    .get_transaction_details(transaction_hash_transfer)
-                    .await
-                    .unwrap()
-                    .unwrap();
+        if details.eth_commit_tx_hash.is_some()
+            && details.eth_prove_tx_hash.is_some()
+            && details.eth_execute_tx_hash.is_some()
+        {
+            break details;
+        }
 
-                if l2_transaction.eth_commit_tx_hash.is_some() {
-                    break l2_transaction.clone();
-                }
-
-                sleep(Duration::from_secs(1)).await; // Adjust the duration as needed
-            }
-        };
-
-        let l2_tx_fee_formatted_transfer = format!("{:#?}", l2_transaction_transfer.fee);
-        println!("L2 fee: {}", l2_tx_fee_formatted_transfer.green());
-
-        let l1_transaction_transfer = l1_rpc_client
-            .get_transaction_by_hash(l2_transaction_transfer.eth_commit_tx_hash.unwrap())
-            .await
-            .unwrap()
-            .unwrap();
-
-        let l1_max_fee_per_gas_transfer = l1_transaction_transfer.max_fee_per_gas.unwrap();
-        let l1_max_fee_per_gas_formatted_transfer = format!("{:#?}", l1_max_fee_per_gas_transfer);
-        println!(
-            "L1 max fee per gas: {}",
-            l1_max_fee_per_gas_formatted_transfer.cyan()
-        );
-        println!();
+        sleep(Duration::from_secs(1)).await;
     }
+}
+
+async fn wait_for_tx_receipt(client: &Provider<Http>, tx_hash: H256) -> TransactionReceipt {
+    loop {
+        let receipt = client
+            .get_transaction_receipt(tx_hash.clone())
+            .await
+            .unwrap();
+        if receipt.is_some() {
+            break receipt.unwrap();
+        }
+        sleep(Duration::from_secs(1)).await;
+    }
+}
+
+async fn tx_gas_used(client: &Provider<Http>, tx_hash: H256) -> U256 {
+    let receipt = wait_for_tx_receipt(client, tx_hash).await;
+    receipt.gas_used.unwrap()
+}
+
+async fn display_gas_and_hash(name: &str, client: &Provider<Http>, tx_hash: H256) {
+    let gas_used = tx_gas_used(client, tx_hash).await;
+    println!("{}", name.bright_red());
+    println!(
+        "Hash: {formatted_hash}",
+        formatted_hash = format!("{tx_hash:#?}").bright_green()
+    );
+    println!(
+        "Gas used: {formatted_gas_used}",
+        formatted_gas_used = format!("{gas_used:#?}").bright_cyan()
+    );
+}
+
+async fn display_tx_details(
+    l1_provider: &Provider<Http>,
+    l2_provider: &Provider<Http>,
+    tx_receipt: TransactionReceipt,
+) {
+    let l2_deploy_tx_details = wait_for_l2_tx_details(tx_receipt.transaction_hash).await;
+
+    display_gas_and_hash("L2 deploy tx", &l2_provider, tx_receipt.transaction_hash).await;
+    display_gas_and_hash(
+        "L1 commit tx",
+        &l1_provider,
+        l2_deploy_tx_details.eth_commit_tx_hash.unwrap(),
+    )
+    .await;
+    display_gas_and_hash(
+        "L1 prove tx",
+        &l1_provider,
+        l2_deploy_tx_details.eth_prove_tx_hash.unwrap(),
+    )
+    .await;
+    display_gas_and_hash(
+        "L1 execute tx",
+        &l1_provider,
+        l2_deploy_tx_details.eth_execute_tx_hash.unwrap(),
+    )
+    .await;
+
+    println!();
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let l1_provider = l1_provider();
+    let l2_provider = l2_provider();
+    let zks_wallet = zks_wallet(&l1_provider, &l2_provider).await;
+
+    // Fund the wallet
+    let _deposit_tx_hash = deposit(&zks_wallet).await;
+
+    // Deploy ERC20 contract
+    let (l2_deployed_contract_address, l2_deploy_tx_receipt) = deploy(&zks_wallet).await;
+    display_tx_details(&l1_provider, &l2_provider, l2_deploy_tx_receipt).await;
+
+    // Mint tokens
+    let l2_mint_tx_receipt = mint(&zks_wallet, l2_deployed_contract_address).await;
+    display_tx_details(&l1_provider, &l2_provider, l2_mint_tx_receipt).await;
+
+    // Transfer tokens
+    let l2_transfer_tx_receipt = transfer(&zks_wallet, l2_deployed_contract_address).await;
+    display_tx_details(&l1_provider, &l2_provider, l2_transfer_tx_receipt).await;
 }
