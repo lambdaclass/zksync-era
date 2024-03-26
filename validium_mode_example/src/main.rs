643852d0d5e96658b782477400c1ab688b6bc743
toni-calvin <antonio.calvin@lambdaclass.com>
2024-03-26 13:52:41 +0100

    add mint_erc20 scenario and transfer_erc20 scenario

--- validium_mode_example/src/main.rs
+++ validium_mode_example/src/main.rs
@@ -1,7 +1,9 @@
-use validium_mode_example::{helpers::TxKind, scenario};
+use validium_mode_example::scenario;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    scenario::run(20, 200, TxKind::Deploy).await;
+    //scenario::run(20, 200, TxKind::Deploy).await;
     //scenario::basic().await;
+    scenario::mint_erc20(5, 5).await;
+    scenario::transfer_erc20(5, 5).await;
 }
