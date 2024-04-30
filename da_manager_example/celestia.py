3b2e676a1e9af65d44d134c17394bfd7c0080846
toni-calvin <antonio.calvin@lambdaclass.com>
2024-04-30 11:55:54 +0200

    running zksync-server and validium_example from script

--- da_manager_example/celestia.py
+++ da_manager_example/celestia.py
@@ -1,61 +1,73 @@
 #!/usr/bin/env python3
 
-import logging
-from subprocess import Popen, PIPE, STDOUT
-import multiprocessing as mp
+import subprocess
+from time import sleep
 
-def log_subprocess_output(pipe, process_name, starts_with, ends_with, matches):
-    for line in iter(pipe.readline, b''): # b'\n'-separated lines
-        line = line.decode("utf-8").strip()
-        if (starts_with != None and line.startswith(starts_with)) or any(line for match in matches if match in line):
-            print(process_name + ": ", line)
-        if ends_with != None and ends_with in line:
-            print("-" * 5 + process_name + " executing." + "-" * 5)
-            if process_name == "Server": 
-                mp.Process(target=execute_example).start()
-            else:
-                print("Process ended")
+RUNNING_SERVER_LOG = "Running `target/release/zksync_server`" 
+SERVER_STEP_CHECK = b'\xe2\x9c\x94'.decode("utf-8")
+
             
-        
+def filter_server_logs(server_log_pipe):
+    for line in iter(server_log_pipe.readline, b''): # b'\n'-separated lines
+        line = line.decode("utf-8").strip()
+        if SERVER_STEP_CHECK in line:
+            print("Server: ", line)
+        if RUNNING_SERVER_LOG in line:
+            sleep(60)
+            print("-" * 5 + "Server is running" + "-" * 5)
+            execute_example()
+        if "INFO" in line:
+            print("Server: ", line)
+
+def filter_example_logs(example_log_pipe):
+    for line in iter(example_log_pipe.readline, b''): # b'\n'-separated lines
+        print("Example: ", line.decode("utf-8").strip())
+
 def init_server():
-    # print("Which zksync server do you want to build?")
-    # print("1. Rollup + Calldata")
-    # print("2. Rollup + Blobs")
-    # print("3. Validium + Calldata")
-    # print("4. Validium + Blobs")
-    # option = int(input("Enter your choice (1-4): "))  
-    # if option == 1:
-    #     process = subprocess.run(["make", "demo_rollup_calldata",  "-C",  "../",])
-    # elif option == 2:
-    #     process = subprocess.run(["make", "demo_rollup_blobs",  "-C",  "../",])
-    # elif option == 3:
-    #     process = subprocess.run(["make", "demo_validium_calldata",  "-C",  "../"])
-    # elif option == 4:
-    #     process = subprocess.run(["make", "demo_validium_blobs",  "-C",  "../"])
-    # else:
-    #     print("Invalid option")
-    command_line_args = ["make", "demo_validium_calldata",  "-C",  "../"]
-    process = Popen(command_line_args, stdout=PIPE, stderr=STDOUT)
-    with process.stdout:
-        server_started = "Running `target/release/zksync_server`"
-        check = b'\xe2\x9c\x94'.decode("utf-8")
-        log_subprocess_output(process.stdout, "Server", ">", server_started, [check])
-    return process.wait() # 0 means success
+    option = select_server()
+    if option == 1:
+        command_line_args = ["make", "demo_rollup_calldata",  "-C",  "../",]
+    elif option == 2:
+        command_line_args = ["make", "demo_rollup_blobs",  "-C",  "../",]
+    elif option == 3:
+        command_line_args = ["make", "demo_validium_calldata",  "-C",  "../"]
+    elif option == 4:
+        command_line_args = ["make", "demo_validium_blobs",  "-C",  "../"]
+    else:
+        print("Invalid option")
+    
+    print ("Building zksync server...")
+    server = subprocess.Popen(command_line_args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    with server.stdout: filter_server_logs(server.stdout)
+    server.wait()
      
 
 def execute_example():
-
+    print("Running example...")
     command_line_args = ["cargo", "run", "--release",  "--bin",  "validium_mode_example"]
-    process = Popen(command_line_args, stdout=PIPE, stderr=STDOUT)
-    with process.stdout:
-        log_subprocess_output(process.stdout, "Example", "Running", None, matches = ["Deposit", "Mint", "Deploy", "Transfer"])
-    return process.wait() # 0 means succes
+    example = subprocess.Popen(command_line_args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    with example.stdout: filter_example_logs(example.stdout)
+    example.wait()
+
+def select_server():
+    print("Which zksync server do you want to build?")
+    print("1. Rollup + Calldata")
+    print("2. Rollup + Blobs")
+    print("3. Validium + Calldata")
+    print("4. Validium + Blobs")
+    return int(input("Enter your choice (1-4):")) 
+
+def get_pubdata():
+    print("Getting pubdata...")
+def init_celestia_server():
+    print("Initializing Celestia server...")
+def submit_data_to_celestia():
+    print("Submitting data to Celestia...")
 
 def main():
-    process_list = []
-    process_list.append(mp.Process(target=init_server))
-    for p in process_list:
-        p.start()
+    init_server()
+    
+
 
 if __name__ == '__main__':
     main()
