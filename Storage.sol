05a53c304e75c35fb3260c5741a7a9f11bf996e8
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2024-08-23 18:01:15 -0300

    Add initial use evm interpreter flag

---
+++ Storage.sol
@@ -0,0 +1,14 @@
+// Storage.sol
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    uint256 private storedData;
+
+    function set(uint256 value) public {
+        storedData = value;
+    }
+
+    function get() public view returns (uint256) {
+        return storedData;
+    }
+}
