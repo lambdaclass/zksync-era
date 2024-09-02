9c63bd0458844e88aa4334a4f4ff3c2208985084
Julian Ventura <julian.ventura@lambdaclass.com>
2024-09-02 12:09:24 -0300

    Fix compiling errors

--- core/tests/vm-benchmark/benches/criterion.rs
+++ core/tests/vm-benchmark/benches/criterion.rs
@@ -14,45 +14,45 @@ use zksync_vm_benchmark_harness::{
 const SAMPLE_SIZE: usize = 20;
 const ZKSYNC_HOME: &str = std::env!("ZKSYNC_HOME");
 
-fn benches_in_folder<VM: BenchmarkingVmFactory, const FULL: bool>(c: &mut Criterion) {
-    let mut group = c.benchmark_group(VM::LABEL.as_str());
-    group
-        .sample_size(SAMPLE_SIZE)
-        .measurement_time(Duration::from_secs(10));
-
-    let benches = format!(
-        "{}/core/tests/vm-benchmark/deployment_benchmarks",
-        ZKSYNC_HOME
-    );
-
-    for path in std::fs::read_dir(&benches).unwrap() {
-        let path = path.unwrap().path();
-
-        let test_contract = std::fs::read(&path).expect("failed to read file");
-
-        let code = cut_to_allowed_bytecode_size(&test_contract).unwrap();
-        let tx = get_deploy_tx(code);
-        let file_name = path.file_name().unwrap().to_str().unwrap();
-        let full_suffix = if FULL { "/full" } else { "" };
-        let bench_name = format!("{file_name}{full_suffix}");
-        group.bench_function(bench_name, |bencher| {
-            if FULL {
-                // Include VM initialization / drop into the measured time
-                bencher.iter(|| BenchmarkingVm::<VM>::default().run_transaction(black_box(&tx)));
-            } else {
-                bencher.iter_batched(
-                    BenchmarkingVm::<VM>::default,
-                    |mut vm| {
-                        let result = vm.run_transaction(black_box(&tx));
-                        (vm, result)
-                    },
-                    BatchSize::LargeInput, // VM can consume significant amount of RAM, especially the new one
-                );
-            }
-        });
-    }
-}
-
+// fn benches_in_folder<VM: BenchmarkingVmFactory, const FULL: bool>(c: &mut Criterion) {
+//     let mut group = c.benchmark_group(VM::LABEL.as_str());
+//     group
+//         .sample_size(SAMPLE_SIZE)
+//         .measurement_time(Duration::from_secs(10));
+//
+//     let benches = format!(
+//         "{}/core/tests/vm-benchmark/deployment_benchmarks",
+//         ZKSYNC_HOME
+//     );
+//
+//     for path in std::fs::read_dir(&benches).unwrap() {
+//         let path = path.unwrap().path();
+//
+//         let test_contract = std::fs::read(&path).expect("failed to read file");
+//
+//         let code = cut_to_allowed_bytecode_size(&test_contract).unwrap();
+//         let tx = get_deploy_tx(code);
+//         let file_name = path.file_name().unwrap().to_str().unwrap();
+//         let full_suffix = if FULL { "/full" } else { "" };
+//         let bench_name = format!("{file_name}{full_suffix}");
+//         group.bench_function(bench_name, |bencher| {
+//             if FULL {
+//                 // Include VM initialization / drop into the measured time
+//                 bencher.iter(|| BenchmarkingVm::<VM>::default().run_transaction(black_box(&tx)));
+//             } else {
+//                 bencher.iter_batched(
+//                     BenchmarkingVm::<VM>::default,
+//                     |mut vm| {
+//                         let result = vm.run_transaction(black_box(&tx));
+//                         (vm, result)
+//                     },
+//                     BatchSize::LargeInput, // VM can consume significant amount of RAM, especially the new one
+//                 );
+//             }
+//         });
+//     }
+// }
+//
 pub fn program_from_file(bin_path: &str) -> Vec<u8> {
     let program = std::fs::read(bin_path).unwrap();
     let encoded = String::from_utf8(program).unwrap();
