b83f4c042a7544f1481f0f23b4e0a32707f152ec
Julian Ventura <julian.ventura@lambdaclass.com>
2024-09-03 17:05:38 -0300

    Separate fibonacci from send benchmarks and re-add full benchmarks

--- core/tests/vm-benchmark/benches/criterion.rs
+++ core/tests/vm-benchmark/benches/criterion.rs
@@ -15,7 +15,8 @@ use zksync_vm_benchmark_harness::{
     pre_calc_address, BenchmarkingVm, BenchmarkingVmFactory, Fast, Lambda, Legacy, LoadTestParams,
 };
 
-const SAMPLE_SIZE: usize = 20;
+const SAMPLE_SIZE: usize = 50;
+const MEASUREMENT_TIME: Duration = Duration::from_secs(20);
 const ZKSYNC_HOME: &str = std::env!("ZKSYNC_HOME");
 
 pub fn program_from_file(bin_path: &str) -> Vec<u8> {
@@ -31,73 +32,121 @@ pub fn program_from_file(bin_path: &str) -> Vec<u8> {
     bin
 }
 
-// Simpler version
-fn benches_in_folder<VM: BenchmarkingVmFactory, const FULL: bool>(c: &mut Criterion) {
+fn bench_fibonacci<VM: BenchmarkingVmFactory, const FULL: bool>(c: &mut Criterion) {
     let mut group = c.benchmark_group(VM::LABEL.as_str());
-
     group
         .sample_size(SAMPLE_SIZE)
-        .measurement_time(Duration::from_secs(10));
+        .measurement_time(MEASUREMENT_TIME);
 
-    let send_bench_tag = "send";
-
-    let send_bench = format!(
+    let bench_tag = "fibonacci_rec";
+    let bench_path = format!(
         "{}/core/tests/vm-benchmark/deployment_benchmarks/{}",
-        ZKSYNC_HOME, send_bench_tag
+        ZKSYNC_HOME, bench_tag
     );
 
-    let fibonacci_bench_tag = "fibonacci_rec";
-    let fibonacci_bench = format!(
-        "{}/core/tests/vm-benchmark/deployment_benchmarks/{}",
-        ZKSYNC_HOME, fibonacci_bench_tag
-    );
-
-    let benches: Vec<(&str, String)> = vec![
-        (send_bench_tag, send_bench),
-        (fibonacci_bench_tag, fibonacci_bench),
-    ];
-
-    let receiver_addr = H160::from_str("0x888888CfAebbEd5554c3F36BfBD233f822e9455f").unwrap();
-    let receiver_balance_key = storage_key_for_eth_balance(&receiver_addr);
-    let sent_value = 100_u32;
-    let expected_receiver_balance: U256 = sent_value.into();
     let expected_fibonacci_result = U256::from_dec_str("75025").unwrap();
+    let bench_name = if FULL {
+        format!("{bench_tag}/full")
+    } else {
+        bench_tag.into()
+    };
+    let code = program_from_file(&bench_path);
+    let tx = get_deploy_tx_with_value(&code[..], 0);
+    let contract_addr = pre_calc_address(&code[..]);
 
-    for (bench_tag, bench_path) in benches {
-        let bench_name = format!("{bench_tag}/full");
-        // Only benchmark the tx execution itself
-        let code = program_from_file(&bench_path);
-        let sent_value = if bench_name.contains("fibonacci_rec") {
-            0
+    group.bench_function(bench_name.clone(), |bencher| {
+        if FULL {
+            // Include VM initialization / drop into the measured time
+            bencher.iter(|| {
+                let mut vm = BenchmarkingVm::<VM>::default();
+                //NOTE: There is a `run_transaction_full` method too. Check if it should be used
+                //here instead.
+                let result = vm.run_transaction(black_box(&tx));
+                assert!(!result.result.is_failed());
+                assert_eq!(
+                    vm.read_storage(contract_addr, H256::zero()),
+                    expected_fibonacci_result
+                );
+            });
         } else {
-            sent_value
-        };
-        let tx = get_deploy_tx_with_value(&code[..], sent_value);
-        let contract_addr = pre_calc_address(&code[..]);
-        group.bench_function(bench_name.clone(), |bencher| {
             bencher.iter_batched(
                 BenchmarkingVm::<VM>::default,
                 |mut vm| {
                     let result = vm.run_transaction(black_box(&tx));
                     assert!(!result.result.is_failed());
-                    if bench_name.starts_with("fibonacci_rec") {
-                        assert_eq!(
-                            vm.read_storage(contract_addr, H256::zero()),
-                            expected_fibonacci_result
-                        );
-                    } else if bench_name.starts_with("send") {
-                        let receiver_balance = vm.read_storage(
-                            *receiver_balance_key.address(),
-                            *receiver_balance_key.key(),
-                        );
-                        assert_eq!(receiver_balance, expected_receiver_balance);
-                    }
+                    assert_eq!(
+                        vm.read_storage(contract_addr, H256::zero()),
+                        expected_fibonacci_result
+                    );
                     (vm, result)
                 },
                 BatchSize::LargeInput,
             );
-        });
-    }
+        }
+    });
+}
+
+fn bench_send<VM: BenchmarkingVmFactory, const FULL: bool>(c: &mut Criterion) {
+    let mut group = c.benchmark_group(VM::LABEL.as_str());
+    group
+        .sample_size(SAMPLE_SIZE)
+        .measurement_time(MEASUREMENT_TIME);
+
+    let bench_tag = "send";
+    let bench_path = format!(
+        "{}/core/tests/vm-benchmark/deployment_benchmarks/{}",
+        ZKSYNC_HOME, bench_tag
+    );
+
+    let sent_value = 100_u32;
+    let receiver_addr = H160::from_str("0x888888CfAebbEd5554c3F36BfBD233f822e9455f").unwrap();
+    let receiver_balance_key = storage_key_for_eth_balance(&receiver_addr);
+    let expected_receiver_balance: U256 = sent_value.into();
+
+    let bench_name = if FULL {
+        format!("{bench_tag}/full")
+    } else {
+        bench_tag.into()
+    };
+    let code = program_from_file(&bench_path);
+    let tx = get_deploy_tx_with_value(&code[..], sent_value);
+    let contract_addr = pre_calc_address(&code[..]);
+
+    group.bench_function(bench_name.clone(), |bencher| {
+        if FULL {
+            // Include VM initialization / drop into the measured time
+            bencher.iter(|| {
+                let mut vm = BenchmarkingVm::<VM>::default();
+                //NOTE: There is a `run_transaction_full` method too. Check if it should be used
+                //here instead.
+                let result = vm.run_transaction(black_box(&tx));
+                assert!(!result.result.is_failed());
+                let receiver_balance =
+                    vm.read_storage(*receiver_balance_key.address(), *receiver_balance_key.key());
+                assert_eq!(receiver_balance, expected_receiver_balance);
+            });
+        } else {
+            bencher.iter_batched(
+                BenchmarkingVm::<VM>::default,
+                |mut vm| {
+                    let result = vm.run_transaction(black_box(&tx));
+                    assert!(!result.result.is_failed());
+                    let receiver_balance = vm
+                        .read_storage(*receiver_balance_key.address(), *receiver_balance_key.key());
+                    assert_eq!(receiver_balance, expected_receiver_balance);
+                    (vm, result)
+                },
+                BatchSize::LargeInput,
+            );
+        }
+    });
+}
+
+fn benches_in_folder<VM: BenchmarkingVmFactory>(c: &mut Criterion) {
+    bench_fibonacci::<VM, false>(c);
+    bench_fibonacci::<VM, true>(c);
+    bench_send::<VM, false>(c);
+    bench_send::<VM, true>(c);
 }
 
 fn bench_load_test<VM: BenchmarkingVmFactory>(c: &mut Criterion) {
@@ -141,14 +190,11 @@ fn bench_load_test_transaction<VM: BenchmarkingVmFactory>(
 
 criterion_group!(
     benches,
-    benches_in_folder::<Fast, false>,
-    benches_in_folder::<Fast, true>,
-    benches_in_folder::<Lambda, false>,
-    benches_in_folder::<Lambda, true>,
-    benches_in_folder::<Legacy, false>,
-    benches_in_folder::<Legacy, true>,
-    bench_load_test::<Fast>,
-    bench_load_test::<Lambda>,
-    bench_load_test::<Legacy>
+    benches_in_folder::<Fast>,
+    benches_in_folder::<Lambda>,
+    benches_in_folder::<Legacy>,
+    // bench_load_test::<Fast>,
+    // bench_load_test::<Lambda>,
+    // bench_load_test::<Legacy>
 );
 criterion_main!(benches);
