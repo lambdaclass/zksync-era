84c3a3b82513e608e44aebc681b313b12af5fd2f
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2024-10-24 16:00:08 -0300

    Add initial verify commitment

--- core/node/eigenda_proxy/src/verifier.rs
+++ core/node/eigenda_proxy/src/verifier.rs
@@ -1,3 +1,10 @@
+use std::str::FromStr;
+
+use ark_bn254::{Fq, G1Affine};
+use rust_kzg_bn254::{blob::Blob, kzg::Kzg};
+
+use crate::common::G1Commitment;
+
 pub enum VerificationError {
     WrongProof,
 }
@@ -20,13 +27,24 @@ pub struct VerifierConfig {
 }
 
 pub struct Verifier {
-    kzg_verifier: bool, // TODO: change this.
+    g1: Vec<G1Affine>, // TODO: change this.
+    kzg: Kzg,
     verify_certs: bool,
     cert_verifier: bool, // TODO: change this.
 }
 
 impl Verifier {
     pub fn new(cfg: VerifierConfig) -> Self {
+        let srs_points_to_load = 2097152 / 32; // 2 Mb / 32 (Max blob size)
+        let kzg = Kzg::setup(
+            "../resources/g1.point",
+            "",
+            "",
+            268435456,
+            srs_points_to_load,
+        );
+        let kzg = kzg.unwrap();
+        let g1: Vec<G1Affine> = kzg.get_g1_points();
         let cert_verifier = if cfg.verify_certs {
             false
             // TODO: create CertVerifier
@@ -38,9 +56,52 @@ impl Verifier {
         //
 
         Self {
-            kzg_verifier: todo!(),
-            verify_certs: cfg.verify_certs,
-            cert_verifier,
+            g1,
+            kzg: kzg,
+            verify_certs: false,
+            cert_verifier: false,
         }
+    }
+
+    fn commit(&self, blob: Vec<u8>) -> G1Affine {
+        let blob = Blob::from_bytes_and_pad(&blob.to_vec());
+        self.kzg.blob_to_kzg_commitment(&blob).unwrap()
+    }
+
+    pub fn verify_commitment(
+        &self,
+        expected_commitment: G1Commitment,
+        blob: Vec<u8>,
+    ) -> Result<(), VerificationError> {
+        let actual_commitment = self.commit(blob);
+        let expected_commitment = G1Affine::new_unchecked(
+            Fq::from(num_bigint::BigUint::from_bytes_be(&expected_commitment.x)),
+            Fq::from(num_bigint::BigUint::from_bytes_be(&expected_commitment.y)),
+        );
+        if actual_commitment != expected_commitment {
+            return Err(VerificationError::WrongProof);
+        }
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    #[test]
+    fn test_verify_commitment() {
+        let verifier = super::Verifier::new(super::VerifierConfig {
+            verify_certs: false,
+            rpc_url: "".to_string(),
+            svc_manager_addr: "".to_string(),
+            eth_confirmation_deph: 0,
+        });
+        let commitment = super::G1Commitment {
+            x: vec![0u8; 32],
+            y: vec![0u8; 32],
+        };
+        let blob = vec![0u8; 32];
+        let result = verifier.verify_commitment(commitment, blob);
+        assert_eq!(result.is_ok(), true);
     }
 }
