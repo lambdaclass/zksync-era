1bac89ad09373d72160b920e3abbd6c08cb46f56
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2024-11-01 17:01:19 -0300

    Add soft confirmations

--- core/node/da_clients/src/eigen/client.rs
+++ core/node/da_clients/src/eigen/client.rs
@@ -125,7 +125,7 @@ mod tests {
             eigenda_svc_manager_address: "0xD4A7E1Bd8015057293f0D0A557088c286942e84b".to_string(),
             blob_size_limit: 2 * 1024 * 1024, // 2MB
             status_query_timeout: 1800,       // 30 minutes
-            status_query_interval: 5,         // 5 seconds
+            status_query_interval: 5,         // 5 ms
             wait_for_finalization: false,
             authenticaded: false,
         });
@@ -154,8 +154,38 @@ mod tests {
             eigenda_svc_manager_address: "0xD4A7E1Bd8015057293f0D0A557088c286942e84b".to_string(),
             blob_size_limit: 2 * 1024 * 1024, // 2MB
             status_query_timeout: 1800,       // 30 minutes
-            status_query_interval: 5,         // 5 seconds
+            status_query_interval: 5,         // 5 ms
             wait_for_finalization: false,
+            authenticaded: true,
+        });
+        let secrets = EigenSecrets {
+            private_key: PrivateKey::from_str(
+                "d08aa7ae1bb5ddd46c3c2d8cdb5894ab9f54dec467233686ca42629e826ac4c6",
+            )
+            .unwrap(),
+        };
+        let client = EigenClient::new(config, secrets).await.unwrap();
+        let data = vec![1; 20];
+        let result = client.dispatch_blob(0, data.clone()).await.unwrap();
+        let blob_info: BlobInfo =
+            rlp::decode(&hex::decode(result.blob_id.clone()).unwrap()).unwrap();
+        // TODO: once get inclusion data is added, check it
+        let retrieved_data = client.get_blob_data(&result.blob_id).await.unwrap();
+        assert_eq!(retrieved_data.unwrap(), data);
+    }
+
+    #[tokio::test]
+    async fn test_wait_for_finalization() {
+        let config = EigenConfig::Disperser(DisperserConfig {
+            custom_quorum_numbers: None,
+            disperser_rpc: "https://disperser-holesky.eigenda.xyz:443".to_string(),
+            eth_confirmation_depth: -1,
+            eigenda_eth_rpc: String::default(),
+            eigenda_svc_manager_address: "0xD4A7E1Bd8015057293f0D0A557088c286942e84b".to_string(),
+            blob_size_limit: 2 * 1024 * 1024, // 2MB
+            status_query_timeout: 1800,       // 30 minutes
+            status_query_interval: 5000,      // 5000 ms
+            wait_for_finalization: true,
             authenticaded: true,
         });
         let secrets = EigenSecrets {
