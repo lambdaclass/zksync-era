ec1a65a2bcd62fcd5ca7d8c6adb4a104a6cade33
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2024-11-01 18:23:54 -0300

    Remove to retriable error

--- core/node/da_clients/src/eigen/client.rs
+++ core/node/da_clients/src/eigen/client.rs
@@ -77,13 +77,6 @@ impl EigenClient {
         }
     }
 }
-
-pub fn to_retriable_error(error: anyhow::Error) -> DAError {
-    DAError {
-        error,
-        is_retriable: true,
-    }
-}
 #[cfg(test)]
 mod tests {
     use zksync_config::configs::da_client::eigen::{DisperserConfig, MemStoreConfig};
